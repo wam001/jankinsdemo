0d84152bb9a0e6d9f130a30097a3674bb38c999b
VonBurrito <hamza.amentag@hotmail.com>
2023-05-03 22:36:14 +0200

    Initial commit

---
+++ src/test/java/com/wam001/jenkins/demo/ServiceTest.java
@@ -0,0 +1,24 @@
+package com.wam001.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * @author Hamza Amentag
+ * @since 5/3/2023
+ */
+public class ServiceTest {
+
+
+    @Test
+    public void isEqualTo(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void notEqualTo(){
+        assertNotEquals(10, 100);
+    }
+}
